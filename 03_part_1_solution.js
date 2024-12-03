28e41fc382363cd7d895440bb72ab373bf368d0d
Eric <eric@spiritsdontfly.com>
2024-12-03 10:06:11 +0100

    add day 3

--- 03_part_1_solution.js
+++ 03_part_1_solution.js
@@ -1,6 +1,17 @@
 const fs = require("fs");
 
 const input = fs.readFileSync("03_input.txt", "utf8");
-const array = input.split("\n");
 
-console.log(array);
+const regex = /mul\(\d{1,3},\d{1,3}\)/g;
+const matches = input.match(regex);
+
+const multiplications = matches.map((match) => {
+  const [_, a, b] = match.match(/mul\((\d{1,3}),\s*(\d{1,3})\)/);
+  return parseInt(a) * parseInt(b);
+});
+
+const sum = multiplications.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sum);
+
+/* 187825547 is correct */
--- 03_part_2_solution.js
+++ 03_part_2_solution.js
@@ -1,6 +1,34 @@
 const fs = require("fs");
 
 const input = fs.readFileSync("03_input.txt", "utf8");
-const array = input.split("\n");
 
-console.log(array);
+const regex = /mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/g;
+const matches = input.match(regex);
+
+let enabled = true;
+const instructions = [];
+
+for (const match of matches) {
+  if (match === "don't()") {
+    enabled = false;
+    continue;
+  } else if (match === "do()") {
+    enabled = true;
+    continue;
+  }
+
+  if (enabled) {
+    instructions.push(match);
+  }
+}
+
+const multiplications = instructions.map((match) => {
+  const [_, a, b] = match.match(/mul\((\d{1,3}),\s*(\d{1,3})\)/);
+  return parseInt(a) * parseInt(b);
+});
+
+const sum = multiplications.reduce((acc, curr) => acc + curr, 0);
+
+console.log(sum);
+
+/* 85508223 is correct */
