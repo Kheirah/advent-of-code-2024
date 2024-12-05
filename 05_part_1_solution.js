254c1a58a3882d1972129b889b9de8eedbf2479a
Eric <eric@spiritsdontfly.com>
2024-12-05 13:38:45 +0100

    add day 5 part 1

--- 05_part_1_solution.js
+++ 05_part_1_solution.js
@@ -3,4 +3,51 @@ const fs = require("fs");
 const input = fs.readFileSync("05_input.txt", "utf8");
 const array = input.split("\n");
 
-console.log(array);
+const rules = [];
+let startPages = -1;
+
+for (let i = 0; i < array.length; i++) {
+  const line = array[i];
+  if (line === "") {
+    startPages = i + 1;
+    break;
+  }
+
+  const [a, b] = line.split("|");
+  rules.push([parseInt(a), parseInt(b)]);
+}
+
+const quickAccessOfOrderingRules = {};
+for (const rule of rules) {
+  const [a, b] = rule;
+  if (quickAccessOfOrderingRules[a] === undefined) {
+    quickAccessOfOrderingRules[a] = [];
+  }
+  quickAccessOfOrderingRules[a].push(b);
+}
+
+const updates = array
+  .slice(startPages)
+  .map((line) => line.split(",").map((page) => parseInt(page)));
+
+const orderedUpdates = updates.map((pages) => {
+  return pages.slice().sort((a, b) => {
+    const rulesOfA = quickAccessOfOrderingRules[a];
+    if (rulesOfA?.includes(b)) {
+      return -1;
+    }
+    return 0;
+  });
+});
+
+let middlePageNumberSum = 0;
+
+for (let i = 0; i < updates.length; i++) {
+  if (orderedUpdates[i].toString() === updates[i].toString()) {
+    middlePageNumberSum += updates[i][Math.floor(updates[i].length / 2)];
+  }
+}
+
+console.log(middlePageNumberSum);
+
+/* 5762 is correct */
